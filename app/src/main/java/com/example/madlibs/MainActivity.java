42fe68ef73ff5172f2f0726f5510c6d1516a4168
201013101 <mekjonathan514@gmail.com>
2023-05-30 11:45:18 -0700

    update gradle

--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.madlibs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -54,11 +55,260 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 playRing();
-
             }
+
         });
 
 
+        btnWildGeese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playWildGeese();
+            }
+
+        });
+
+        btnArcade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playArcade();
+            }
+
+        });
+
+        btnDonkeyScream.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playDonkeyScream();
+            }
+
+        });
+
+        btnSword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSword();
+            }
+
+        });
+
+        btnMonkey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playMonkey();
+            }
+
+        });
+
+        btnRobot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playRobot();
+            }
+
+        });
+
+        btnThunder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playThunder();
+            }
+
+        });
+
+        btnGuitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playGuitar();
+            }
+
+        });
+
+        btnWhistle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playWhistle();
+            }
+
+        });
+
+        btnToy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playToy();
+            }
+
+        });
+
+        btnBird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBird();
+            }
+
+        });
+    }
+
+
+
+
+
+
+        private void playRing() {
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_ring);
+            mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.stop();
+                    mp.release();
+                    mp = null;
+                }
+            });
+        }
+
+    private void playWildGeese() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_geese);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playArcade() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_arcade);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playDonkeyScream() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_donkey);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playSword() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_sword);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playMonkey() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_monkey);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playRobot() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_alien);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playThunder() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_thunder);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playGuitar() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_guitar);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playWhistle() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_whistle);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playToy() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_toy);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playBird() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_jon_bird);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
     }
 }
 
